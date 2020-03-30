d3903fb80d04e9db8669a301a80232563699c947
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-31 02:32:19 +0530

    ADDED: Calculating wages till woking hours or days is reached for a month

--- employeeWages.sh
+++ employeeWages.sh
@@ -11,22 +11,32 @@ Present=1;
 
 EmpMonthWage=0;
 Days=0;
+Hours=0;
 
-while [[ $Days -ne 20 ]]
+while [[ $Days -ne 20 && $Hours -le 100 ]]
 do
 	CheckAttendance=$((RANDOM%3));
-	case $CheckAttendance in
+
+case $CheckAttendance in
+
 		$Present)
 			EmpHour=8;
+			Hours=$((Hours+$EmpHour))
 			;;
+
 		$PartTimePresent)
 			EmpHour=4;
+			Hours=$((Hours+$EmpHour))
 			;;
 		*)
 			EmpHour=0;
 			;;
+
 	esac
-	
+
 	((Days++))
+	DailyWage=$((EmpHour*WagePerHour))
+
 	EmpMonthWage=$((EmpHour*WagePerHour+EmpMonthWage))
+
 done
