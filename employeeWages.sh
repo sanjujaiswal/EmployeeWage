f58ee0ce06cf89a089b4e58602d088fcca0061e9
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-03-31 01:48:02 +0530

    ADDED: Part time employee and wages

--- employeeWages.sh
+++ employeeWages.sh
@@ -5,19 +5,21 @@ echo "Welcome To Employee Wage Computation Program"
 #Declaration of contants and variables
 
 WagePerHour=20;
-FullDayHour=8;
 Present=1;
+PartTimePresent=2;
 
-CheckAttendance=$((RANDOM%2));
+CheckAttendance=$((RANDOM%3));
 
 if [ $CheckAttendance -eq $Present ]
 then
-		echo "Employee is Present"
-		employeeDailyWage=$((WagePerHour*FullDayHour))
-		echo $employeeDailyWage
+	EmpHour=8;
+
+elif [ $CheckAttendance -eq $PartTimePresent ]
+then
+	 EmpHour=4;
 
 else
-		echo "Employee is Absent"
-		employeeDailyWage=0;
-
+	EmpHour=0;
 fi
+
+employeeDailyWage=$((EmpHour*WagePerHour));
