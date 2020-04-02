05fdffcf2ab1c32215cfb231481dd770e5b9d77e
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-02 18:40:56 +0530

    REFACTOR: Added function to calculate monthly total working hours and wages

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,49 +1,71 @@
-#!/bin/bash -x
-
+#!/bin/bash
 echo "Welcome To Employee Wage Computation Program"
 
 #Declaration of contants
-WagePerHour=20;
-DayInMonth=20;
+WAGE_PER_HOUR=20;
+DAY_IN_MONTH=20;
+NUMBER=3;
+NUM_WORKING_DAYS=20;
+WORK_HRS_IN_MONTH=100;
 
 #Declaration of variables
-Present=1;
-PartTimePresent=2;
-EmpMonthWage=0;
-EmpHour=0;
-Days=0;
-Hours=0;
+present=1;
+partTimePresent=2;
+empMonthWage=0;
+empHour=0;
+days=0;
+hours=0;
+checkAttendance=0;
+dailyWage=0;
+totalAmount=0;
+totalWorkingHr=0;
 
 function getWorkHr(){
-	case $1 in
-		$Present)
-			EmpHour=8;
-			echo $EmpHour;
+case $1 in
+		$present)
+			empHour=8;
+			echo $empHour;
 			;;
-		$PartTimePresent)
-			EmpHour=4;
-			echo $EmpHour;
+		$partTimePresent)
+			empHour=4;
+			echo $empHour;
 			;;
 		*)
-			EmpHour=0;
-			echo $EmpHour;
+			empHour=0;
+			echo $empHour;
 			;;
-	esac
+esac
 }
 
+# To check number of working days and total working hours in a month
 
-while [[ $Days -ne 20 && $Hours -le 100 ]]
+function wagesInMonth(){
+
+#
+
+while [[ $days -ne NUM_WORKING_DAYS && $hours -le $WORK_HRS_IN_MONTH ]]
 do
-		CheckAttendance=$((RANDOM%3));
-		EmpHour=$( getWorkHr $CheckAttendance )
-		Hours=$((Hours+$EmpHour))
+		checkAttendance=$((RANDOM%$NUMBER));
+		empHour=$( getWorkHr $checkAttendance )
+		hours=$(($hours+$empHour))
 
-		DailyWage=$((EmpHour*WagePerHour))
-		DailyWage[$Days]=$DailyWage;
-		((Days++))
+		#calculate employee's daily wages
+		dailyWage=$((empHour*WAGE_PER_HOUR))
+		dailyWage[$days]=$dailyWage;
+		((days++))
 
-		EmpMonthWage=$((DailyWage+EmpMonthWage))
+		#calculate employee's monthly wages
+		empMonthWage=$((dailyWage+empMonthWage))
+		empMonthwage[$days]=$empMonthwage;
+
+		#calculate total amount of employee's
+		totalAmount=$(($totalAmount+$empMonthWage))
 
 done
+}
 
-echo -e "${!DailyWage[@]} :\n ${DailyWage[@]}"
+#Function calling
+wagesInMonth
+echo -e "Days :${!dailyWage[@]} \nDaily Wages: ${dailyWage[@]}"
+echo "Total amount in month :$totalAmount"
+echo "Total working hours in month :$hours"
