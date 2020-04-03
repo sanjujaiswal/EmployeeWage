32f4acc42440f1b8feab401cc451943f8e441a12
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-03 13:49:53 +0530

    REFACTOR: changes done,indentation follows

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome To Employee Wage Computation Program"
 
 #Declaration of contants
@@ -21,51 +21,50 @@ totalAmount=0;
 totalWorkingHr=0;
 
 function getWorkHr(){
-case $1 in
-		$present)
-			empHour=8;
-			echo $empHour;
-			;;
-		$partTimePresent)
-			empHour=4;
-			echo $empHour;
-			;;
-		*)
-			empHour=0;
-			echo $empHour;
-			;;
-esac
+	case $1 in
+	$present)
+		empHour=8;
+		echo $empHour;
+		;;
+	$partTimePresent)
+		empHour=4;
+		echo $empHour;
+		;;
+	*)
+		empHour=0;
+		echo $empHour;
+		;;
+	esac
 }
 
 # To check number of working days and total working hours in a month
-
 function wagesInMonth(){
-
-#
 
 while [[ $days -ne NUM_WORKING_DAYS && $hours -le $WORK_HRS_IN_MONTH ]]
 do
-		checkAttendance=$((RANDOM%$NUMBER));
-		empHour=$( getWorkHr $checkAttendance )
-		hours=$(($hours+$empHour))
+	checkAttendance=$((RANDOM%$NUMBER));
+	empHour=$( getWorkHr $checkAttendance )
+	hours=$(($hours+$empHour))
 
-		#calculate employee's daily wages
-		dailyWage=$((empHour*WAGE_PER_HOUR))
-		dailyWage[$days]=$dailyWage;
-		((days++))
+	#calculate employee's daily wages
+	dailyWage=$((empHour*WAGE_PER_HOUR))
+	dailyWage[$days]=$dailyWage;
+	((days++))
 
-		#calculate employee's monthly wages
-		empMonthWage=$((dailyWage+empMonthWage))
-		empMonthwage[$days]=$empMonthwage;
-
-		#calculate total amount of employee's
-		totalAmount=$(($totalAmount+$empMonthWage))
-
+	#calculate employee's monthly wages
+	empMonthWage=$((dailyWage+empMonthWage))
+	empMonthwage[$days]=$empMonthwage;
 done
+}
+
+function tatalWageCal(){
+totalAmount=$(($dailyWage+$empMonthWage))
+totalAmount[$days]=$totalAmount;
 }
 
 #Function calling
 wagesInMonth
+tatalWageCal
 echo -e "Days :${!dailyWage[@]} \nDaily Wages: ${dailyWage[@]}"
 echo "Total amount in month :$totalAmount"
 echo "Total working hours in month :$hours"
